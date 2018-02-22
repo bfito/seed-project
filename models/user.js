0199167ea7b86256fe0135add707e1e2deafa555
JPZ <jeanpaulzune@gmail.com>
2018-02-21 22:21:20 -0500

    Created me some models

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+var schema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, requeried: true},
+    password: {type: String, requeried: true},
+    email: {type: String, requeried: true, unique: true},
+    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
+  });
+
+  schema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('User', schema);
