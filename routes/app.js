3d871f2c29815111bc52c2bb050e08ffc66d8358
JPZ <jeanpaulzune@gmail.com>
2018-02-23 15:02:00 -0500

    Everything should be good. No mayor changes made. Commiting before doing a update to package dependencies in suspicion mongoose, mongodb and node incompatibility headache...

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+
 router.get('/', function (req, res, next) {
+    User.findOne({}, function(err, doc) {
+      if (err) {
+        return res.send('Error!');
+      }
+      res.render('node', {email: doc});
+    });
     res.render('node');
 });
 
