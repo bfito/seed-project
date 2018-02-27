89e697b279f3d836d5c1ac1d971a7a6b298fc06c
JPZ <jeanpaulzune@gmail.com>
2018-02-26 22:17:23 -0500

    Setting up the service

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
+
+import { MessageService } from "./message.service";
+
 @Component({
   selector: 'app-message-input',
-  templateUrl: './message-input.component.html'
+  templateUrl: './message-input.component.html',
+  providers: []
 })
 
 export class MessageInputComponent {
+  constructor(private messageService: MessageService) {}
   onSave(value: string) {
     console.log(value);
   }
