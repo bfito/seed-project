c5ab0910c7d27c720301135227e06b775a979a97
JPZ <jeanpaulzune@gmail.com>
2018-03-01 14:47:33 -0500

    Setting up signin, signout, logout initial files no logic yet

---
+++ assets/app/auth/signin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './auth/signin.component.html'
+})
+
+export class SigninComponent {
+
+}
---
+++ assets/app/auth/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './auth/signup.component.html'
+})
+
+export class SignupComponent {
+
+}
