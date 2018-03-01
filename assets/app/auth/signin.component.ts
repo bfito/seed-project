2a1ac6063fd6437a92b959627ac0eb1dd515b136
JPZ <jeanpaulzune@gmail.com>
2018-03-01 17:57:16 -0500

    Started User Signup with Reactive (Data-Driven) Forms

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html'
 })
 
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+  myForm: FormGroup;
 
+  ngOnInit() {
+    this.myForm = new FormGroup({
+
+    });
+  }
 }
