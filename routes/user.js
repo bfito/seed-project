f58eac6e90d90982bf332fca83d282efbd690cd5
JPZ <jeanpaulzune@gmail.com>
2018-03-23 15:21:44 -0400

    Signup looking good. Had to correct directory path to user model for user router. All seems good...

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
 
-var User = require('./models/user');
+var User = require('../models/user');
 
 router.post('/', function (req, res, next) {
       var user = new User({
