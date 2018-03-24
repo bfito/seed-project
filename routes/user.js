1802a377dd08ed8e48d71e859e06a200c9754379
JPZ <jeanpaulzune@gmail.com>
2018-03-23 21:33:44 -0400

    Worked on login. And just install jsonwebtoken

--- routes/user.js
+++ routes/user.js
@@ -26,4 +26,26 @@ router.post('/', function (req, res, next) {
       });
 });
 
-module.exports = router;
+router.post('/signin', function(req, res, next){
+    User.findOne({email: req.body.email}, function(err, user){
+      if (err) {
+        // we use return to return this message and to return to begginning of function
+        return res.status(500).json({
+          title: 'An error occured',
+          error: err
+        });
+      } if(!user) {
+          return res.status(401).json({
+            title: 'No user found',
+            error: {message: 'Invalid Login Credentials'}
+          });
+      }
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        return res.status(401).json({
+          title: 'Login failed',
+          error: {message: 'Invalid login Credentials'}
+        });
+      }
+      
+    });
+});
